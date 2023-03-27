3d483811ab1880dec89d489d29199e7294f1f08c
Michel Maillard <info@maillard.li>
2023-03-27 18:32:48 +0200

    1. Installation de base

---
+++ eleventy.config.js
@@ -0,0 +1,7 @@
+const eleventySass = require("eleventy-sass");
+
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPlugin(eleventySass);
+
+  return { dir: { input: "src", output: "dist" } };
+};
