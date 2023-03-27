d122b474155cbba7421651592a717edca245a2c2
Michel Maillard <info@maillard.li>
2023-03-27 18:50:53 +0200

    3. Modifier le style

--- eleventy.config.js
+++ eleventy.config.js
@@ -2,6 +2,7 @@ const eleventySass = require("eleventy-sass");
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(eleventySass);
+  eleventyConfig.addPassthroughCopy("src/js");
 
   return { dir: { input: "src", output: "dist" } };
 };
